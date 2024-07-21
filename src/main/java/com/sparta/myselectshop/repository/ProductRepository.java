e10d98477d5cd4dcde2413e736e7cf7190f065cf
bongdaehyun <pkjeogus@gmail.com>
2024-07-21 16:49:52 +0900

    관심상품 최저가 수정 API

--- src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
+++ src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
@@ -3,5 +3,8 @@ package com.sparta.myselectshop.repository;
 import com.sparta.myselectshop.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<Product,Long> {
+    Optional<Product> findByTitle(String title);
 }
