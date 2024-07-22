65b94c4a15a6596497b5e16f879e53d06f77c060
bongdaehyun <pkjeogus@gmail.com>
2024-07-22 15:22:38 +0900

    폴더 생성 및 회원별 조회

---
+++ src/main/java/com/sparta/myselectshop/service/FolderService.java
@@ -0,0 +1,63 @@
+package com.sparta.myselectshop.service;
+
+
+import com.sparta.myselectshop.dto.FolderResponseDto;
+import com.sparta.myselectshop.entity.Folder;
+import com.sparta.myselectshop.entity.User;
+import com.sparta.myselectshop.repository.FolderRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class FolderService {
+
+    private final FolderRepository folderRepository;
+
+    @Transactional
+    public void addFolder(List<String> folders, User user)
+    {
+        List<Folder> existingFolders = folderRepository.findAllByUserAndNameIn(user,folders);
+
+        List<Folder> newFolders = new ArrayList<>();
+
+        for(String folderName : folders)
+        {
+            if(!isExistedFolder(folderName,existingFolders))
+            {
+                Folder folder = new Folder(folderName,user);
+                newFolders.add(folder);
+            }else{
+                throw new IllegalArgumentException("폴더명이 중복되었습니다.");
+            }
+        }
+
+        folderRepository.saveAll(newFolders);
+    }
+
+    private boolean isExistedFolder(String folderName, List<Folder> existingFolders) {
+        for(Folder folder : existingFolders) {
+            if (folderName.contains(folder.getName())) {
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+    // 로그인한 회원이 등록된 모든 폴더 조회
+    public List<FolderResponseDto> getFolders(User user) {
+        List<Folder> folderList = folderRepository.findAllByUser(user);
+        List<FolderResponseDto> responseDtoList = new ArrayList<>();
+
+        for (Folder folder : folderList) {
+            responseDtoList.add(new FolderResponseDto(folder));
+        }
+
+        return responseDtoList;
+    }
+}
