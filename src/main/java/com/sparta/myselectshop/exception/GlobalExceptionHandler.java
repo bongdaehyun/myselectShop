005229bf70b170e90b5a2b66c66896af23cd1951
bongdaehyun <pkjeogus@gmail.com>
2024-07-29 15:35:28 +0900

    error 메시지 관리

--- src/main/java/com/sparta/myselectshop/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sparta/myselectshop/exception/GlobalExceptionHandler.java
@@ -5,20 +5,39 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-//공통된 에러핸들러를 작성 및 관리하기 편하다.
-//controller마다 핸들러를 작성하기에는 비용이 많이 들어간다.
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler({IllegalArgumentException.class})
-    public ResponseEntity<RestApiException> handleException(IllegalArgumentException ex) {
+    public ResponseEntity<RestApiException> illegalArgumentExceptionHandler(IllegalArgumentException ex) {
         RestApiException restApiException = new RestApiException(ex.getMessage(), HttpStatus.BAD_REQUEST.value());
-
         return new ResponseEntity<>(
                 // HTTP body
                 restApiException,
                 // HTTP status code
                 HttpStatus.BAD_REQUEST
+        );
+    }
+
+    @ExceptionHandler({NullPointerException.class})
+    public ResponseEntity<RestApiException> nullPointerExceptionHandler(NullPointerException ex) {
+        RestApiException restApiException = new RestApiException(ex.getMessage(), HttpStatus.NOT_FOUND.value());
+        return new ResponseEntity<>(
+                // HTTP body
+                restApiException,
+                // HTTP status code
+                HttpStatus.NOT_FOUND
+        );
+    }
+
+    @ExceptionHandler({ProductNotFoundException.class})
+    public ResponseEntity<RestApiException> notFoundProductExceptionHandler(ProductNotFoundException ex) {
+        RestApiException restApiException = new RestApiException(ex.getMessage(), HttpStatus.NOT_FOUND.value());
+        return new ResponseEntity<>(
+                // HTTP body
+                restApiException,
+                // HTTP status code
+                HttpStatus.NOT_FOUND
         );
     }
 }
